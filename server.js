41753781f52c1ee181b979341e20d085f4ccbe7c
blaham <master.sartory@gmail.com>
2025-04-25 20:40:19 +0200

    Nevím-fakt-už

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 require("dotenv").config();
-const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
-const manifest = require("./manifest");
+const { serveHTTP } = require("stremio-addon-sdk");
+const addonInterface = require("./index");
+const PORT = process.env.PORT || 7000;
 
-// Vytvoříme builder a definujeme handlery (catalog a stream jsme už dřív implementovali ve index.js,
-// případně sem můžete rovnou přesunout ten kód z index.js)
-const { defineCatalogHandler, defineStreamHandler } = require("./index-handlers"); 
-// index-handlers.js exportuje funkci, která vrátí builder s nadefinovanými handlery:
-//   module.exports = (manifest, token) => {
-//     const builder = new addonBuilder(manifest);
-//     builder.defineCatalogHandler(...);
-//     builder.defineStreamHandler(...);
-//     return builder;
-//   };
-
-const builder = defineCatalogHandler(manifest, process.env.WST);
-const addonInterface = builder.getInterface();
-
-// Spustíme HTTP server, který:
-– servíruje manifest na /manifest.json
-– obslouží /catalog/... a /stream/... automagicky
-serveHTTP(addonInterface, {
-  port: process.env.PORT || 7000,
+process.on("unhandledRejection", (reason) => {
+  console.error("❌ Nezachycená promise:", reason);
 });
 
-// Volitelné logování startu
-console.log(`🚀 Addon běží na portu ${process.env.PORT || 7000}`);
+serveHTTP(addonInterface, { port: PORT });
+console.log(`🚀 Addon běží na portu ${PORT}`);
